c5a19030d7902f71fc5cb8f7a2c0dd6a3db560dd
rossdraper <rossdraper@users.noreply.github.com>
2024-05-18 13:03:15 +0800

    Update recv.py

--- recv.py
+++ recv.py
@@ -8,31 +8,28 @@ PORT = 3000
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Extract the Base64 encoded part from the URL path
-        encoded_string = self.path[1:]  # skip the leading '/'
+        encoded_string = self.path[1:] 
         try:
-            # Attempt to decode the Base64 encoded string
             decoded_data = base64.urlsafe_b64decode(encoded_string)
 
-            # Check if the data is gzipped by looking at the first two bytes
             if decoded_data[:2] == b'\x1f\x8b':
                 with gzip.GzipFile(fileobj=BytesIO(decoded_data)) as gzip_file:
                     decoded_string = gzip_file.read().decode('utf-8')
-                print("Decoded and Unzipped URL Argument:", decoded_string)
+                print(decoded_string, end='')
             else:
                 decoded_string = decoded_data.decode('utf-8')
-                print("Decoded URL Argument:", decoded_string)
+                print(decoded_string, end='')
         except Exception as e:
             print("Error decoding and/or unzipping Base64:", e)
-
-        # Send a 404 response
         self.send_response(404)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-#        self.wfile.write(b"404 Not Found")
+
+    def log_message(self, format, *args):
+        pass
 
 class MyTCPServer(socketserver.TCPServer):
-    # This allows the server to bind to the address and port even if it was recently used
+
     allow_reuse_address = True
 
 def run(server_class=MyTCPServer, handler_class=MyHTTPHandler):
